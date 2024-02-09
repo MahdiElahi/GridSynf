92f1d80c3a91c606b0b120da944fd142e50981c2
MahdiElahi <32883870+MahdiElahi@users.noreply.github.com>
2024-02-09 04:02:36 +0330

    Add files via upload

---
+++ BookService.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Application.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly IBookRepository BookRepository;
+
+        public BookService(IBookRepository BookRepository)
+        {
+            this.BookRepository = BookRepository;
+        }
+  
+        public async Task<FilterResult<BookViewModel>> GetAllAsync(FilterViewModel filterViewModel)
+        {
+            var mapped = ObjectMapper.Mapper.Map<FilterResult<BookViewModel>>(await BookRepository.GetAllAsync(filterViewModel));
+            return  mapped;
+        }
+
+        public async Task<List<string>> GetStatus()
+        {
+            return await BookRepository.GetStatus();
+        }
+
+        public async Task UpdateAsync(BookViewModel viewModel)
+        {
+            var mapped = ObjectMapper.Mapper.Map<Book>(viewModel);
+            await BookRepository.UpdateAsync(mapped);
+        }
+      
+    }
+}
