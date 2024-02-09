980b54bf7f9da954acaf517f0def620d5333b7ec
MahdiElahi <32883870+MahdiElahi@users.noreply.github.com>
2024-02-09 03:59:57 +0330

    Add files via upload

---
+++ BooksAdaptor.cs
@@ -0,0 +1,44 @@
+﻿
+namespace BookFinder.Adaptors
+{
+    public class BookAdaptor : DataAdaptor
+    {
+        private readonly IBookService BookService;
+        public BookAdaptor(IBookService BookService)
+        {
+            this.BookService = BookService;
+        }
+
+        public override async Task<object> ReadAsync(DataManagerRequest dataManagerRequest, string additionalParam = null)
+        {
+            FilterViewModel filteringViewModel = new();
+
+            List<WhereFilter> wheres = dataManagerRequest.Where ?? new List<WhereFilter>();
+
+            if (wheres.Any())
+            {
+                var perdicates = wheres.FirstOrDefault().predicates;
+                filteringViewModel.Where = perdicates;
+            }
+
+            if (dataManagerRequest.Sorted is not null)
+            {
+                filteringViewModel.SortBy = dataManagerRequest.Sorted[0].Name;
+                filteringViewModel.SortDirection = dataManagerRequest.Sorted[0].Direction;
+            }
+            filteringViewModel.Skip = dataManagerRequest.Skip;
+            filteringViewModel.Take = dataManagerRequest.Take;
+
+            var result = await BookService.GetAllAsync(filteringViewModel);
+
+
+            DataResult dataResult = new DataResult
+            {
+                Count = result.Count,
+                Result = result.Result
+            };
+
+            return dataResult;
+        }
+    }
+}
